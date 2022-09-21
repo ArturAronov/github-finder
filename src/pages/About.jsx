de5d4a52c267b84eefd38bf2931abf38a059c0c5
ArturAronov <unionco@icloud.com>
2022-09-21 13:05:56 +0800

    updated About page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,22 +1,28 @@
 function About(){
   return(
     <div>
-      <h1 className='text-6xl mb-4'>
-        GitHub finder
+      <h1 className='text-2xl mb-4'>
+        About the Github Finder App
       </h1>
-      <p className='mb-4 text-2xl font-light'>
-        A React app to search GitHub profiles and see profile details. This
-        project is part of the
-        <a href='https://www.udemy.com/course/modern-react-front-to-back/'>
-          {' '}
-          React Front To Back
-        </a>{' '}
-        Udemy course by
-        <strong>
-          <a href='https://traversymedia.com'> Brad Traversy</a>
-        </strong>
-        .
+      <p className='mb-1 text-xl font-light'>
+        This website was created by Artur Aronov, for purpose to practice the following stack:
       </p>
+      <ul className="list-disc mb-5">
+        <li>React</li>
+        <li>TailwindCSS</li>
+        <li>DaisyUI</li>
+        <li>Github API</li>
+        <li>Vercel (Deployment)</li>
+      </ul>
+
+      <p className='mb-1 text-xl font-light'>
+        Socials:
+      </p>
+      <ul className="list-disc mb-5">
+        <li><a href="https://www.linkedin.com/in/artaro/" target="_blank" rel="noopener noreferrer">LinkedIn</a>.</li>
+        <li><a href="https://github.com/ArturAronov/github-finder" target="_blank" rel="noopener noreferrer">Github Repo</a>.</li>
+        <li><a href="http://www.artur.hk" target="_blank" rel="noopener noreferrer">Portfolio</a>.</li>
+      </ul>
     </div>
   );
 };
